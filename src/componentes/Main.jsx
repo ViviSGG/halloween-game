f7c2ab8ead9ebf0d9857dbd66eb2760ae1e94f38
Lívia V. Gomes <114452531+ViviSGG@users.noreply.github.com>
2023-10-14 20:27:34 -0300

    add style

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -5,49 +5,54 @@ import Fantasma from "../assets/img/fantasma.png";
 import Pirata from "../assets/img/pirata.png";
 import Vampiro from "../assets/img/vampiro.png";
 import Zumbi from "../assets/img/zumbi.png";
+import "./style.css"
 
 function Index() {
-  const [conteudo, setConteudo] = useState([
-    {
-      nome: "Bruxa",
-      imagem: Bruxa
-    },
-    {
-      nome: "Fada",
-      imagem: Fada
-    },
-    {
-      nome: "Fantasma",
-      imagem: Fantasma
-    },
-    {
-      nome: "Pirata",
-      imagem: Pirata
-    },
-    {
-      nome: "Vampiro",
-      imagem: Vampiro
-    },
-    {
-      nome: "Zumbi",
-      imagem: Zumbi
-    }
-  ]);
-  return (
-    <main>
-      <h1>Halloween VnW</h1>
-      <section>
-        {/* map - é um método que recebe até 3 parametros */}
-        {conteudo.map((
-          elemento /* sem parametro ele conta quantos elementos tem dentro, com parametro ele tem acesso para puxar um elemento de dentro */
-        ) => (
-          <figure>
-            <img src={elemento.imagem} alt={elemento.nome}/>
-            <figcaption>{elemento.nome}</figcaption>
-          </figure>
-        ))}
-      </section>
-    </main>
-  );
+    const [conteudo, setConteudo] = useState([
+        {
+        nome: "Bruxa",
+        imagem: Bruxa
+        },
+        {
+        nome: "Fada",
+        imagem: Fada
+        },
+        {
+        nome: "Fantasma",
+        imagem: Fantasma
+        },
+        {
+        nome: "Pirata",
+        imagem: Pirata
+        },
+        {
+        nome: "Vampiro",
+        imagem: Vampiro
+        },
+        {
+        nome: "Zumbi",
+        imagem: Zumbi
+        }
+    ]);
+    return (
+        <main>
+            <h1>Halloween VnW</h1>
+            <section>
+                {/* map - é um método que recebe até 3 parametros */}
+                {conteudo.map((elemento) => ( /* sem parametro ele conta quantos elementos tem dentro, com parametro ele tem acesso para puxar um elemento de dentro */
+                <figure>
+                    <img src={elemento.imagem} alt={elemento.nome}/>
+                    <figcaption>{elemento.nome}</figcaption>
+                </figure>
+                ))}
+                {conteudo.map((elemento) => ( 
+                <figure>
+                    <img src={elemento.imagem} alt={elemento.nome}/>
+                    <figcaption>{elemento.nome}</figcaption>
+                </figure>
+                ))}
+            </section>
+        </main>
+    );
 }
 export default Index;
